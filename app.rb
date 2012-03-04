38854c42a24599ca372da0140ac26e8a6e04613d
David Rankin <Rankida@gmail.com>
2012-03-04 21:19:38 +0000

    First commit in a long while so dont know all that has changed, but the index is now my basic JSON editor.

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 require 'rubygems'
 require 'sinatra'
+require "sinatra/content_for"
+require 'json'
 
 get "/" do
-  "<h1>Welcome to <i>Rankida</i>.com</h1><br />Light the fuse..."
+  erb :jsonEdit
+end
+
+get '/jsonEditor' do
+  erb :jsonEdit
 end
 
 get "/hello/:name" do |n|
   "<h1>Hello " + n + "</h1><br />Light the fuse..."
+end
+
+get "/blueprint" do 
+  erb :launchpad
 end
