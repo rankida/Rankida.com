3b0a466776742a3b86e0cfa69a2591c9ed451ca4
David Rankin <Rankida@gmail.com>
2016-04-02 16:35:10 +0100

    work in progress, but getting there

--- assets/js/loop.js
+++ assets/js/loop.js
@@ -104,11 +104,19 @@ jQuery( function($){
 	/* PARALLAX
 	/*---------------------*/
 
+	// .parallax(xPosition, speedFactor, outerHeight) options:
+	// xPosition - Horizontal position of the element
+	// inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	// outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+
 	$('.full-width-parallax').parallax(0, 0.1);
 
 	function setParallax() {
 		if( $(window).width() > 1024 ) {
 			$('.full-width-parallax').parallax(0, 0.1);
+			if( $('#testimonial').hasClass('parallax') ) {
+				$('#testimonial').parallax(0, 0.1);
+			}
 		}
 	}
 
