f6a824ccd0c67c338ee2dd1bdb1f4c326f3c9a0b
David Rankin <Rankida@gmail.com>
2016-04-18 12:40:53 +0100

    Added formspree to js

--- assets/js/loop.js
+++ assets/js/loop.js
@@ -148,44 +148,6 @@ jQuery( function($){
 		offset: '70%'
 	});
 
-
-	/*----------------------/
-	/* TWITTER STREAM
-	/*---------------------*/
-
-	/*
-	* ### HOW TO CREATE A VALID ID TO USE: ###
-	* Go to www.twitter.com and sign in as normal, go to your settings page.
-	* Go to "Widgets" on the left hand side.
-	* Create a new widget for what you need eg "user timeline" or "search" etc.
-	* Feel free to check "exclude replies" if you dont want replies in results.
-	* Now go back to settings page, and then go back to widgets page, you should
-	* see the widget you just created. Click edit.
-	* Now look at the URL in your web browser, you will see a long number like this:
-	* 345735908357048478
-	* Use this as your ID below instead!
-	*/
-	/**
-	* How to use fetch function:
-	* @param {string} Your Twitter widget ID.
-	* @param {string} The ID of the DOM element you want to write results to.
-	* @param {int} Optional - the maximum number of tweets you want returned. Must
-	*     be a number between 1 and 20.
-	* @param {boolean} Optional - set true if you want urls and hashtags to be hyperlinked!
-	* @param {boolean} Optional - Set false if you dont want user photo /
-	*     name for tweet to show.
-	* @param {boolean} Optional - Set false if you dont want time of tweet
-	*     to show.
-	* @param {function/string} Optional - A function you can specify to format
-	*     tweet date/time however you like. This function takes a JavaScript date
-	*     as a parameter and returns a String representation of that date.
-	*     Alternatively you may specify the string 'default' to leave it with
-	*     Twitter's default renderings.
-	*/
-
-	twitterFetcher.fetch( '441767385733668865', 'tweet', 1, true, false, true, 'default');
-
-
 	/*----------------------/
 	/* SCROLL TO TOP
 	/*---------------------*/
@@ -306,7 +268,12 @@ jQuery( function($){
 		$btn.find('.loading-icon').addClass('fa-spinner fa-spin ');
 		$btn.prop('disabled', true).find('span').text("Sending...");
 
-		$.post('contact.php', $(this).serialize(), function(data){
+		$.ajax({
+		    url: $theForm.attr('action'),
+		    method: "POST",
+		    data: $(this).serialize(),
+		    dataType: "json"
+		}).then(function(data){
 
 			$message = data.message;
 
@@ -324,7 +291,6 @@ jQuery( function($){
 
 		})
 		.fail(function() { console.log('AJAX Error'); });
-
 	});
 
 
